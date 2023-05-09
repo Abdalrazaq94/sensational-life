88f3e97e4988085a17aca52ca629556be5935a1d
Abd@94 <45979612+Abdalrazaq94@users.noreply.github.com>
2023-05-09 20:28:26 +0100

    test

--- client/webpack/common.config.js
+++ client/webpack/common.config.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-dupe-keys */
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
@@ -12,34 +11,30 @@ module.exports = {
 				use: {
 					loader: "babel-loader",
 					options: {
-						cacheDirectory: true
-					}
-				}
+						cacheDirectory: true,
+					},
+				},
 			},
 			{
 				test: /\.(png|svg|jpe?g|gif)$/,
-				loader: "file-loader"
+				loader: "file-loader",
 			},
 			{
 				test: /\.css$/,
-				use: ["style-loader", "css-loader"]
-			}
-		]
+				use: ["style-loader", "css-loader"],
+			},
+		],
 	},
 	output: {
 		publicPath: "/",
-		filename: "bundle.js",
-		// eslint-disable-next-line no-undef
-		path: path.resolve(__dirname, "dist"),
-		publicPath: process.env.PUBLIC_URL || "/",
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			favicon: "./client/src/favicon.ico",
-			template: "./client/src/index.html"
-		})
+			template: "./client/src/index.html",
+		}),
 	],
 	resolve: {
-		enforceExtension: false
-	}
+		enforceExtension: false,
+	},
 };
